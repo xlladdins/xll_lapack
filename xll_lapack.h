3c16111de30ffc6c1ea70ebcf2189b264c9e04d4
Keith A. Lewis <keithalewis@live.com>
2022-02-21 16:40:37 -0500

    sync

--- xll_lapack.h
+++ xll_lapack.h
@@ -1,5 +1,37 @@
-// xll_template.h - common includes
+// xll_lapack.h - LAPACK for Excel
 #pragma once
-// Uncomment to build for pre 2007 Excel
-//#define XLOPERX XLOPER
 #include "xll/xll/xll.h"
+
+// non-owning vector
+inline blas::vector<double> fpvector(_FPX* pa)
+{
+	if (size(*pa) == 1 and pa->array[0] != 0) {
+		handle<blas::vector<double>> v(pa->array[0]);
+		if (v) {
+			return *v.ptr();
+		}
+	}
+
+	return blas::vector<double>(size(*pa), pa->array, 1);
+}
+// non-owning matrix
+inline blas::matrix<double> fpmatrix(_FPX* pa)
+{
+	static const std::type_info& fp_array = typeid(_FPX);
+	static const std::type_info& fpx_array = typeid(FPX);
+	static const std::type_info& blas_matrix = typeid(blas::matrix<double>);
+
+	if (size(*pa) == 1 and pa->array[0] != 0) {
+		if (const auto p = to_pointer<_FPX>(pa->array[0]); typeid(*p) == fp_array) {
+			return blas::matrix<double>(p->rows, p->columns, p->array);
+		}
+		if (const auto p = to_pointer<FPX>(pa->array[0]); typeid(*p) == fpx_array) {
+			return blas::matrix<double>(p->rows(), p->columns(), p->array());
+		}
+		if (const auto p = to_pointer<blas::matrix<double>>(pa->array[0]); typeid(*p) == blas_matrix) {
+			return *p;
+		}
+	}
+
+	return blas::matrix<double>(pa->rows, pa->columns, pa->array);
+}
